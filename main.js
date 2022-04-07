70cef8842f2a1bc17efefa51b3a6e4e27e991dcc
AntonNechaevStudent <anton.m.nechaev@gmail.com>
2022-04-07 19:32:32 +0500

    lesson2

--- main.js
+++ main.js
@@ -1,40 +1,99 @@
-let t = Number(prompt ('input temp'));
-alert (`Цельсий: ${t}, Фаренгейт: ${t * (9 / 5) + 32}`);
-let name = "Anton";
-let admin = name;
-console.log (admin);
-/*
-1. 4 умножаем на 2 получаем 8.
-2. 3 минус 8 получаем -5.
-3. С помощью console.log выводим число -5 в консоль.
-*/
-console.log(3 - 4 * 2);
-/*
-1. складываем 10+10=20
-2. справа прибавляем строчную 10
-3. выводим строку 2010
-*/
-console.log(10 + 10 + "10");
-/*
-1. получаем строчную 1010
-2. справа к ней прибавляем число 10
-3. выводим строку 101010
-*/
-console.log(10 + "10" + 10);
-/*
-1. складываем 10+10=20
-2. унарный плюс преобразует строку 10 в число 10
-3. складываем 20+10=30
-4. выводим 30
-*/
-console.log(10 + 10 + +"10");
-/*
-1. унарный минус преобразует пустую строку в -0
-2. делим 10 на -0
-3. получаем infinity
-*/
-console.log(10 / -"");
-/*
-1. унарный + пытается преобразовать строку в число но из за , получает NaN
-*/
-console.log(10 / +"2,5");
+
+// let a = 1, b = 1, c, d;
+
+//   /*
+//   инкремент увеличивает a на 1 с=2
+//    */
+//   c = ++a;
+//   console.log(c);
+
+//   /*
+//   постфиксная форма возвращает старое значение до инкремента d=1
+//    */
+//   d = b++;
+//   console.log(d);
+
+//   /*
+//   значение a = 2 после предыдущего, ++a добавляет еще 1 с=5
+//    */
+//   c = 2 + ++a;
+//   console.log(c);
+
+//   /*
+//   постфиусная форма в предыдущем примере увеличила b и теперь b=3 но в этом примере постфиксная форма передаёт предыдущее значение b=2 поэтому d=4
+//    */
+//   d = 2 + b++;
+//   console.log(d);
+
+//   console.log(a);
+//   console.log(b);
+
+  //2
+  
+//   "use strict";
+
+//   let a = 2;
+//   let x = 1 + (a *= 2);
+
+//   /*
+//   a *= 2 равносильно a = a*2, то есть переменной a присвоили новое значение 
+//   x = 1 + 4
+
+//    */
+//   console.log(x);
+//   console.log(a);
+
+  // //3
+  // const a = Number(prompt (`Введите a`));
+  // const b = Number(prompt (`Введите b`));
+  
+  // function result(c,d) {
+  //   if (c>=0 && d>=0) {
+  //     console.log(c-d)
+  //   } else if (c<=0 && d<=0) {
+  //     console.log(c*d)
+  //   } else if ((c>0 && d<0) || (c<0 && d>0)) {
+  //     console.log(c+d)
+  //   }
+  // }
+  // result(a,b);
+  // //4
+  // const sum = (a, b) =>  a + b;
+  // const raznost = (a,b) => {
+  //   if (a >= b) {
+  //     return(a - b)} 
+  //     else { 
+  //       return(b - a) }};
+  // const del = (a,b) => a/b;
+  // const umnog = (a,b) => a*b;
+  
+  // console.log(sum (1, 10));
+  //5
+  function declinationOfNumber(count, one, two, five) {
+    const lastDigit = count % 10;
+    switch (lastDigit) {
+      case 0: return (five);
+      case 1: return (one);
+      case 2: return (two);
+      case 3: return (two);
+      case 4: return (two);
+      case 5: return (five);
+      case 6: return (five);
+      case 7: return (five);
+      case 8: return (five);
+      case 9: return (five);
+      default:
+        break;
+    }
+  } 
+  const applesNumber = 15553;
+  // Получаем от функции одно из слов "яблоко", "яблока" или "яблок".
+  const word = declinationOfNumber(applesNumber, "яблоко", "яблока", "яблок");
+  // Выводим корректную строку, например:
+  // "У меня есть 1 яблоко."
+  // "У меня есть 2 яблока."
+  // "У меня есть 5 яблок."
+  // При любом целом, положительном числе в applesNumber, строка, которая
+  // выводится в console.log, должна иметь правильное склонение слова "яблоко".
+  console.log(`У меня есть ${applesNumber} ${word}.`);
+
